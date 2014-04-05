1801ba643259ccaa4c80226fcc20734441f0615e
enricogallinucci <e.gallinucci@gmail.com>
2014-04-05 15:28:02 +0200

    First commit

---
+++ js/100.js
@@ -0,0 +1,64 @@
+var count = 1;
+
+$( document ).ready(function() {
+	$('.cell').each( function(){
+		$(this).addClass("clickable");
+	});
+	$('.clickable').click(click);
+	
+	$('.restart').click(restart);
+});
+
+function click(){
+		$(this).removeClass("clickable");
+		$(this).off('click');
+		$(this).addClass("clicked");	
+			
+		$('.clickable').each( function(){
+			$(this).removeClass("clickable");
+			$(this).off('click');
+		});
+		
+		$(this).html(count); 
+		$('#bar').width(count+"%");
+		count++; 
+		if(count>100){
+			$('#game-win').show();
+			return;
+		}
+		
+		var x = +$(this).attr('id').substr(0,1);
+		var y = +$(this).attr('id').substr(1,1);
+		
+		if($('#'+(x+3)+y).html()=="") $('#'+(x+3)+y).addClass("clickable");
+		if($('#'+(x+2)+(y+2)).html()=="") $('#'+(x+2)+(y+2)).addClass("clickable");
+		if($('#'+(x)+(y+3)).html()=="") $('#'+(x)+(y+3)).addClass("clickable");
+		if($('#'+(x-2)+(y+2)).html()=="") $('#'+(x-2)+(y+2)).addClass("clickable");
+		if($('#'+(x-3)+y).html()=="") $('#'+(x-3)+y).addClass("clickable");
+		if($('#'+(x-2)+(y-2)).html()=="") $('#'+(x-2)+(y-2)).addClass("clickable");
+		if($('#'+(x)+(y-3)).html()=="") $('#'+(x)+(y-3)).addClass("clickable");
+		if($('#'+(x+2)+(y-2)).html()=="") $('#'+(x+2)+(y-2)).addClass("clickable");
+		
+		var con = false;
+		$('.clickable').each( function(){
+			$(this).addClass("clickable");
+			$(this).click(click);
+			con = true;
+		});
+		if(!con){
+			$('#game-over').show();
+		}
+}
+
+function restart(){
+	$('.game-message').each( function(){ $(this).hide(); });
+	$('.cell').each( function(){
+		$(this).removeClass("clicked");
+		$(this).off('click');
+		$(this).html("");
+		$(this).addClass("clickable");
+		count = 1;
+		$('#bar').width("0%");
+	});
+	$('.clickable').click(click);
+}
